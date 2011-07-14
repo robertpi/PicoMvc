12d2d342161580587a99aac1b6f30a5d0d09002e
Robert Pickering <robert@strangelights.com>
2011-07-14 16:36:15 +0200

    Add new get files.

---
+++ nugetpackage/content/Global.fs
@@ -0,0 +1,23 @@
+﻿namespace PicoMvc.Demo
+
+open System
+open System.IO
+open System.Web
+open System.Web.Routing
+open System.Reflection
+open Microsoft.FSharp.Reflection
+open Strangelights.PicoMvc
+
+type Global() =
+    inherit System.Web.HttpApplication() 
+
+    static member RegisterRoutes(routes:RouteCollection) =
+        let routingTables = RoutingTable.LoadFromCurrentAssemblies()
+        let actions =
+            { TreatParameterAction = [  ]
+              TreatResultAction = [  ] }
+        routes.Add(new Route("{*url}", new PicoMvcRouteHandler("url", routingTables, actions)))
+
+    member x.Start() =
+        log4net.Config.XmlConfigurator.Configure()
+        Global.RegisterRoutes(RouteTable.Routes)
